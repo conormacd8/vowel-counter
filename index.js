d79d15b87be9ff1ccc90ec90aaf16a2badb5c4d6
Conor MacDonald <conormacd8@gmail.com>
2022-06-20 12:52:05 +1000

    Added js functionality

---
+++ index.js
@@ -0,0 +1,16 @@
+const userInput = document.getElementById('user-input')
+const countBtn = document.getElementById('count-btn')
+
+countBtn.addEventListener('click', function(){
+  let vowelCount = 0
+  if (userInput.value){
+    let textArray = userInput.value.split('')
+    console.log(textArray)
+    textArray.forEach(letter => {
+      if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+        vowelCount += 1
+      }
+    })
+    alert(vowelCount)
+  }
+})
